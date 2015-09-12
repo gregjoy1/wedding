0a96843ee2ab8b0a25381425f4b489c664faa3a0
Greg Joy <gregory@incuna.com>
2015-09-12 19:49:16 +0100

    Made session controller tests much more robust

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe SessionController, type: :controller do
     )
   end
 
+  after(:all) do
+    @guest.destroy!
+  end
+
   context 'is_logged_in route' do
     context 'when not logged in' do
       it 'returns 403' do
@@ -20,19 +24,36 @@ RSpec.describe SessionController, type: :controller do
 
   context 'login route' do
 
-    it 'returns 400 when posting incorrect params' do
+    it 'returns appropriately when posting incorrect params' do
       post :login, {}.to_json, 'CONTENT_TYPE' => 'application/json'
+
+      data = JSON.parse(response.body)
+
       expect(response.status).to eql(400)
+      expect(data['error']).to eql(['Bad Request'])
+      expect(data['data']).to eql({})
     end
 
-    it 'returns 403 when posting incorrect login credentials' do
+    it 'returns appropriately when posting incorrect login credentials' do
       post :login, {'password' => 'wrong password'}.to_json, 'CONTENT_TYPE' => 'application/json'
+
+      data = JSON.parse(response.body)
+
       expect(response.status).to eql(403)
+      expect(data['error']).to eql(['Incorrect Password'])
+      expect(data['data']).to eql({})
     end
 
-    it 'returns 200 when posting correct login credentials' do
+    it 'returns appropriately when posting correct login credentials' do
       post :login, {'password' => 'test'}.to_json, 'CONTENT_TYPE' => 'application/json'
+
+      data = JSON.parse(response.body)
+
       expect(response.status).to eql(200)
+      expect(data['error']).to eql([])
+      expect(data['data']['guest']['id']).to eql(@guest.id)
+      expect(data['data']['guest']['name']).to eql(@guest.name)
+      expect(data['data']['guest']['rspv']).to eql(@guest.rspv)
     end
 
   end
