1cebca0164c8770aa2f13235d9c6bd124c1343d3
Greg Joy <gregory@incuna.com>
2015-09-15 21:16:07 +0100

    Added hash helper spec + sync

---
+++ spec/lib/helpers/hash_helper_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+require "#{Rails.root.join('lib')}/helpers/hash_helper.rb"
+
+RSpec.describe SessionHelper do
+  include HashHelper
+
+  before(:all) do
+  end
+
+  context 'password_hash' do
+    before(:all) do
+      @password_hash_returned = HashHelper.password_hash("test")
+    end
+
+    it('returns hash correctly') do
+      expect(@password_hash_returned).to eql("72e3ae3d5fed6f34a114fb0558684245b7c11c8938ea8d1982103c20af6dbc44")
+    end
+  end
+
+  context 'interleave_strings' do
+    before(:all) do
+      @interleave_strings_returned = HashHelper.interleave_strings("hahahahah", "yes hello")
+    end
+
+    it('returns interleaved strings correctly') do
+      expect(@interleave_strings_returned).to eql("hyaehsa hhaehlalho")
+    end
+  end
+end
+
+
