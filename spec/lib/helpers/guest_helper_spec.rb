5dd03b7d0d31a803b4c56aad463cc4dcc9d61c51
Greg Joy <gregory@incuna.com>
2015-09-15 21:09:33 +0100

    Added guest helper spec

---
+++ spec/lib/helpers/guest_helper_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+require "#{Rails.root.join('lib')}/helpers/guest_helper.rb"
+
+RSpec.describe SessionHelper do
+  include GuestHelper
+
+  before(:all) do
+    @guest = Guest.create(
+      :name => 'Test Guest',
+      :password => HashHelper.password_hash('test')
+    )
+  end
+
+  after(:all) do
+    @guest.destroy!
+  end
+
+  context 'serialize_guest' do
+    before(:all) do
+      @serialize_guest_returned = GuestHelper.serialize_guest(@guest)
+    end
+
+    it('returns guest correctly') do
+      expect(@serialize_guest_returned[:id]).to eql(@guest.id)
+      expect(@serialize_guest_returned[:name]).to eql(@guest.name)
+      expect(@serialize_guest_returned[:rspv]).to eql(@guest.rspv)
+    end
+  end
+end
+
